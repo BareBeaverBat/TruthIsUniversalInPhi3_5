94d6626980add91d8224c013101f69270abd0d06
Scott Salisbury <scott.s.salisbury@tsgforce.com>
2025-02-13 13:36:15 -0500

    delete obsolete code

--- tiu_phi_3_5_mini/phi_3_5_constants.py
+++ tiu_phi_3_5_mini/phi_3_5_constants.py
@@ -9,18 +9,11 @@ seed = 1894327
 
 dsets_folder = Path("./true_false_datasets")
 
-#TODO remove all references to these (plus delete the files themselves)
-train_split_records_path = dsets_folder / "train_split_record_indices.json"
-validation_split_records_path = dsets_folder / "validation_split_record_indices.json"
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 directions_results_folder = Path("./learned_vectors")
 finalized_activations_dir = Path("D:\\TruthIsUniversal_In_Phi_3_5_Mini\\best2_layers_activations_for_final_token_of_sequences")
 dsets_index_path = dsets_folder / "datasets_index.csv"
-
-misc_datasets_index_path = dsets_folder / "misc_dsets_index.json"
-four_way_topics_index_path = dsets_folder / "four_way_dsets_index.json"
 
 probes_folder = Path("./trained_probes")
 baseline_probes_folder = Path("./baseline_linear_probes")
